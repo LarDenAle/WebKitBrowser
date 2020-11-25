3dd3d22fa9731e7a526e9269593cf99ffaa2046d
Denis Larin <ilardenale@gmail.com>
2020-11-25 17:06:24 +0300

    Initial Commit

---
+++ WebKitBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebKitBrowser
+//
+//  Created by MacOS on 25.11.2020.
+//  Copyright © 2020 Denis Larin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
